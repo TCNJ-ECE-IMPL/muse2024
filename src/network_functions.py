1b0c6856ee2872854ab157f4f9cf24a728adf35c
DJ Landau <newuser@zelda.lions.tcnj.edu>
2024-06-25 13:50:55 -0400

    Fixed some issues with switching to catagorical

--- src/network_functions.py
+++ src/network_functions.py
@@ -9,10 +9,15 @@ INPUT_SIZE = 1024
 USE_FFT = True
 CLASS_NAMES = ['Secure', 'Compromised']
 
+N_TRAIN_EXAMPLES = 250
+N_VALID_EXAMPLES = 50
+BATCHSIZE = 30
+
 # reads all tdms files in directory and adds them to an array of tuples containing input and output
 def get_base_dataset(dir):
-    # dataset contains tuples of (input_data, output_data)
-    dataset = []
+    x = np.empty([1, 1, INPUT_SIZE], dtype=float)
+    y = np.empty([1], dtype=int)
+
 
     # Get data from each tdms file in directory
     for tdms_filename in os.listdir(dir):
@@ -28,12 +33,10 @@ def get_base_dataset(dir):
         index = 0
 
         while index + INPUT_SIZE * 2 < total_samples:
-            # FIXME
-            # Currently setting all output data to 0, we will decide how to convey this when we develop the physical testing system further
             if tdms_filename == "secure.tdms":
-                output_data = 'Secure'
+                output_data = 0
             else:
-                output_data = 'Compromised'
+                output_data = 1
 
             # Input data is the fft of the selected portion of the wave input
             if USE_FFT:
@@ -42,36 +45,46 @@ def get_base_dataset(dir):
                 input_data_unfiltered = channel_data[(index) : (index + INPUT_SIZE * 2)]
             
             input_data = filterHighest(input_data_unfiltered)
+            input_data = np.array([input_data])
 
             # Add data to the dataset
-            data_tuple = (input_data, output_data)
-            dataset.append(data_tuple)
-            
+            x = np.append(x, [input_data], axis=0)
+            y = np.append(y, [output_data], axis=0)
+
             # Increment index and repeat for next section
             index = index + INPUT_SIZE * 2
     
-    return dataset
+    x = np.delete(x, 0, axis=0)
+    y = np.delete(y, 0, axis=0)
 
-# Gets the dataset and formats to 
+    return (x, y)
+
+def unison_shuffled_copies(a, b):
+    assert len(a) == len(b)
+    p = np.random.permutation(len(a))
+    return a[p], b[p]
+
 def get_dataset(dir):
-    # Get dataset from tdms files
-    dataset = get_base_dataset(dir)
-    # Shuffle dataset
-    random.shuffle(dataset)
+    (x, y) = get_base_dataset(dir)
+    (x, y) = unison_shuffled_copies(x, y)
 
     # Partition dataset
     train_frac = 0.7
     valid_frac = 0.2
 
-    train_end_index = math.floor(len(dataset) * train_frac)
-    valid_end_index = train_end_index + math.floor(len(dataset) * valid_frac)
+    train_end_index = math.floor(len(x) * train_frac)
+    valid_end_index = train_end_index + math.floor(len(x) * valid_frac)
 
-    (x_train, y_train) = tuplelist2listtuple(dataset[0:train_end_index])
-    (x_valid, y_valid) = tuplelist2listtuple(dataset[train_end_index + 1 : valid_end_index])
-    (x_test, y_text) = tuplelist2listtuple(dataset[valid_end_index + 1 : len(dataset)])
+    x_train = x[0:train_end_index]
+    y_train = y[0:train_end_index]
+    x_valid = x[train_end_index + 1 : valid_end_index]
+    y_valid = y[train_end_index + 1 : valid_end_index]
 
     train_ds = tf.data.Dataset.from_tensor_slices((x_train, y_train))
+    train_ds = train_ds.shuffle(60000).batch(BATCHSIZE).take(N_TRAIN_EXAMPLES)
+
     valid_ds = tf.data.Dataset.from_tensor_slices((x_valid, y_valid))
+    valid_ds = valid_ds.shuffle(10000).batch(BATCHSIZE).take(N_VALID_EXAMPLES)
     return train_ds, valid_ds
 
 # Turns the shuffleable list of tuples to an easier to work with tuple of lists
