7419f3a1efd6d14aa2a65f69f1d87db7a219462a
franbinoaldrino <faldridge071@gmail.com~>
2024-07-08 14:02:59 -0400

    Merge branch 'main' of https://github.com/TCNJ-ECE-IMPL/muse2024 idek

--- src/plot_log.py
+++ src/plot_log.py
@@ -1,60 +1,106 @@
 import matplotlib.pyplot as plt
+import numpy as np
+from scipy.signal import spectrogram
+import nptdms
 import re
 import ast
+import os
 
-# Plots a .log function created from network_optuna
+def tdms_to_numpy(tdms_path):
+    # Find data array from tdms
+    tdms_file = nptdms.TdmsFile.read(tdms_path)
+    group = tdms_file["Group Name"]
+    channel = group["Voltage_0"]
+    channel_data = channel[:]
+    return channel_data
 
-###########################
-##   Reading .log file   ##
-###########################
+def plot_log_file(log_path):
+    # Plots a .log function created from network_optuna
 
-## Trial [value] finished with value: [decimal value] and parameters: [new dictionary of parameters]
-trial_format = re.compile(r"Trial (\d+) finished with value: ([\d\.]+) and parameters: ({.*?}).")
+    ###########################
+    ##   Reading .log file   ##
+    ###########################
 
-## Modify this to change log path
-log_path = "src/study.log"
+    ## Trial [value] finished with value: [decimal value] and parameters: [new dictionary of parameters]
+    trial_format = re.compile(r"Trial (\d+) finished with value: ([\d\.]+) and parameters: ({.*?}).")
 
-## Open log
-file = open(log_path,"r")
+    ## Modify this to change log path
+    if log_path == None:
+        log_path = "src/study.log"
 
-## Skip first line; this can be modified to make use of the study name
-first_line = file.readline
+    ## Open log
+    file = open(log_path,"r")
 
-## Create trials array
-trials = []
+    ## Skip first line; this can be modified to make use of the study name
+    first_line = file.readline
 
-## Parse log file and store trial info
-for line in file:
-    curr_trial = trial_format.search(line)              ## Check if line follows format
-    if curr_trial:                                      ## If yes...
-        trial_num = int(curr_trial.group(1))                ## Gather trial number, finished value, and parameters
-        val = float(curr_trial.group(2))
-        param = ast.literal_eval(curr_trial.group(3))
+    ## Create trials array
+    trials = []
 
-        trial_details = {                               ## Group our info, assign tags
-            "trial_num" : trial_num,
-            "val" : val,
-            "param" : param
-        }
+    ## Parse log file and store trial info
+    for line in file:
+        curr_trial = trial_format.search(line)              ## Check if line follows format
+        if curr_trial:                                      ## If yes...
+            trial_num = int(curr_trial.group(1))                ## Gather trial number, finished value, and parameters
+            val = float(curr_trial.group(2))
+            param = ast.literal_eval(curr_trial.group(3))
 
-        trials.append(trial_details)                    ## Append to trials array
+            trial_details = {                               ## Group our info, assign tags
+                "trial_num" : trial_num,
+                "val" : val,
+                "param" : param
+            }
 
-######################
-##   Create graph   ##
-######################
+            trials.append(trial_details)                    ## Append to trials array
 
-## Assign axes
-x_axis = [trial["trial_num"] for trial in trials]
-y_axis = [trial["val"] for trial in trials]
+    ######################
+    ##   Create graph   ##
+    ######################
 
-## Plotting 
-plt.plot(x_axis, y_axis)
- 
-## Assign labels and title
-plt.xlabel('Trial Number')
-plt.ylabel('Value')
-plt.title('Test')
- 
-# Display plot
-plt.grid(True, which="both", ls="--")
-plt.show()
+    ## Assign axes
+    x_axis = [trial["trial_num"] for trial in trials]
+    y_axis = [trial["val"] for trial in trials]
+
+    ## Plotting 
+    plt.plot(x_axis, y_axis)
+    
+    ## Assign labels and title
+    plt.xlabel('Trial Number')
+    plt.ylabel('Value')
+    plt.title('Test')
+    
+    # Display plot
+    plt.grid(True, which="both", ls="--")
+    plt.show()
+
+def plot_spectrogram(tdms_path):
+    fs = 12800  # Sample rate, may or may not be correct
+
+    array = tdms_to_numpy(tdms_path)
+
+    # Compute the spectrogram
+    frequencies, times, Sxx = spectrogram(array, fs)
+
+    # Plot the spectrogram
+    plt.figure(figsize=(10, 6))
+    plt.pcolormesh(times, frequencies, 10 * np.log10(Sxx))
+    plt.ylabel('Frequency [Hz]')
+    plt.xlabel('Time [sec]')
+    plt.title('Spectrogram')
+    plt.colorbar(label='Intensity [dB]')
+    plt.show()
+    
+    return
+
+if __name__ == "__main__":
+    while True:
+        file_path = input("Enter full path to desired tdms file: ")
+        
+        if os.path.isfile(file_path):
+            print("File path is valid. Generating spectrogram...")
+            break
+        else:
+            print("File path is not valid. Please try again.")
+            
+    plot_spectrogram(file_path)
+    
